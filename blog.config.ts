11e3bd4cbc5638579154c3dd567cee8cfebb1361
理塘丁真 <nitousei@gmail.com>
2025-07-03 22:36:52 +0800

    删除友链

--- blog.config.ts
+++ blog.config.ts
@@ -8,15 +8,15 @@ export { zhCN as dateLocale } from 'date-fns/locale/zh-CN'
 // 存储 nuxt.config 和 app.config 共用的配置
 // 此处为启动时需要的配置，启动后可变配置位于 app/app.config.ts
 const blogConfig = {
-    title: '纸鹿摸鱼处',
+    title: 'Nitousei',
     subtitle: '纸鹿至麓不知路，支炉制露不止漉',
     // 长 description 利好于 SEO
-    description: '纸鹿本鹿的个人博客，分享技术与生活。“折腾不止，摸鱼生活——摸门🙏🏻”。纸鹿是一名开源爱好者，结识了许多志同道合的朋友。这个博客记录了他在生活和技术学习中的点滴经历，充满启发与思考。网站界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
+    description: '',
     author: {
         name: '纸鹿本鹿',
         avatar: 'https://www.zhilu.cyou/api/avatar.png',
-        email: 'hi@zhilu.cyou',
-        homepage: 'https://www.zhilu.cyou/',
+        email: 'nitousei@gmail.com',
+        homepage: 'https://blog.nitousei.cn/',
     },
     copyright: {
         abbr: 'CC BY-NC-SA 4.0',
@@ -25,10 +25,10 @@ const blogConfig = {
     },
     favicon: 'https://www.zhilu.cyou/api/icon.png',
     language: 'zh-Hans',
-    qqGroup: '169994096',
+    qqGroup: '',
     timeEstablished: '2019-07-19',
     timezone: 'Asia/Shanghai',
-    url: 'https://blog.zhilu.cyou/',
+    url: 'https://blog.nitousei.cn/',
 
     feed: {
         limit: 50,
